b3a443907c87656f20bd60cc48940e87ce361928
CHARLIE <jcarlosbotero1@outlook.com>
2024-04-07 00:03:47 -0400

    Update main.cpp

--- MULTI22/src/main.cpp
+++ MULTI22/src/main.cpp
@@ -2,21 +2,21 @@
  * Script by: Juan Carlos Botero
  * Date: 04/06/2024
  * ==============================================================================
- *                         Copyright (c) 2024 MAIN 12 LLC
+ *                   Copyright (c) 2024 MAIN 12 LLC
  * ==============================================================================
  * ========================      CONVEYOR MULTI-22      =========================
  * ========================   Multiplo Colombia S.A.S.  =========================
  * ==============================================================================
- *  Description:
- *  This is the main code of the control structure of the conveyor MULTI-19 is
- *  intend to be compiled whit Visual Studio Code using the Platformio plug, this
- *  code works by itself, including all the external libraries in the project path
- *  in the "lib" folder as well as the auxiliary functions and reusable resources
- *  in the "include" folder.
+ * Description:
+ * This script serves as the primary control structure for the CONVEYOR MULTI-22,
+ * developed by Multiplo Colombia S.A.S. It is designed to be compiled with Visual
+ * Studio Code using the PlatformIO plugin. The code is self-contained, including
+ * all necessary external libraries located in the "lib" folder, as well as 
+ * auxiliary functions and reusable resources in the "include" folder.
  *
- *  The proposed programming architecture is based on object-oriented finite state
- *  machines the architecture documentation can be found attached to the project
- *  path, otherwise request directly from Charlie projects.
+ * The programming architecture is built upon object-oriented finite state machines.
+ * For detailed documentation on the architecture, please refer to the attached
+ * documentation within the project folder, or reach out directly to Main12 LLC.
  *
  * ==============================================================================
  * MIT License
