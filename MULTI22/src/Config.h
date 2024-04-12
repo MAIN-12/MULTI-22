432e1077a38f39ebbeec3549d01e695fc0dcc712
CHARLIE <jcarlosbotero1@outlook.com>
2024-04-12 19:00:48 -0400

    Update Config.h

--- MULTI22/src/Config.h
+++ MULTI22/src/Config.h
@@ -38,7 +38,7 @@
 // }
 
 // =============== CODE VERSION =================================================
-const String version = "3.0.3";
+const String version = "3.0.4";
 char update[] = "04-12-2024";
 char ID[] = "XXXX001MULTI22";
 
